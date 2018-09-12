123d1ab8b5641ba7ed259ef6e0aeae2677afbf23
tyang21 <42978521+tyang21@users.noreply.github.com>
2018-09-12 14:07:11 -0400

    Add files via upload

---
+++ src/SmileyFace.java
@@ -0,0 +1,22 @@
+
+/**
+ * 
+ * @author Tyler Yang
+ * Smiley Face
+ * Period 7
+ *
+ */
+public class SmileyFace 
+{
+
+	public static void main(String[] args) 
+	{System.out.println("\\\\\\\\\\\\");
+	System.out.println("| * * |");
+	System.out.println("|  *  |");
+	System.out.println("| \\_/ |");
+	System.out.println("\\__ __/");
+		// TODO Auto-generated method stub
+
+	}
+
+}
